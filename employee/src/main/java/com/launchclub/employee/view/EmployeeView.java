7d8151cc38a7408b00a33c5c3e7052be661207f4
KarthickVijaykumar <vkarthick4199@gmail.com>
2022-07-26 17:28:05 +0530

    changes branch 2

--- employee/src/main/java/com/launchclub/employee/view/EmployeeView.java
+++ employee/src/main/java/com/launchclub/employee/view/EmployeeView.java
@@ -288,7 +288,7 @@ public class EmployeeView {
 		try {
 			EMPLOYEE_CONTROLLER.deleteEmployee(employeeId);
 			LOGGER.info("Data Deleted Successfully!!!");
-			System.out.println("hii pakki");
+			System.out.println("hii drtfgyhujikjihgfdsaWQWESDRFTGYHUJK");
 		} catch (CustomException exception) {
 			LOGGER.error(exception);
 			EmployeeView.deleteEmployee();
